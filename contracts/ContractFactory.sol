f86aa1f34981133cf196d680564196eb6c08bd0c
cmonlloroltra <c.monlloroltra@edu.gva.es>
2024-05-09 16:11:26 +0200

    recoded ContractFactory because was deleted someway... I hate git

---
+++ contracts/ContractFactory.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+abstarct contract ContractFactory {
+    fucntion create2Contract(
+        bytes memory bytecode,
+        bytes32 salt
+    ) internal returns (address addr) {
+        assembly {
+            addr := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+    }
+}
